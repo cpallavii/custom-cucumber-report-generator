9b5341aaa6322b7c9ab02a26e3a2e40bd4aa7210
Wendy Lam <mwxlam@gmail.com>
2019-04-02 14:27:36 +1100

    update unit tests and fix to get coverage working in package.json

--- unit.test.js
+++ unit.test.js
@@ -1,35 +1,46 @@
-const {getOptions, getArgs} = require('./utils')
+const { getOptions, getArgs } = require('./utils')
 describe('Unit test for custom report generator', () => {
   describe('Validate different parameters', () => {
-    const origProcessArgs = process.argv
 
     test('Error message is returned when incorrect number of parameters passed in', () => {
-      origProcessArgs.shift()
+      // reset process.argv to known state
+      process.argv = []
       expect(getArgs).toThrow('Incorrect number of parameters called, expect "-f <filename>" OR -i <json>')
     })
 
     test('Error is returned for missing parameters', () => {
-      origProcessArgs.push('')
+      // reset process.argv to known state
+      process.argv = []
+      process.argv.push('', '', '')
       expect(getArgs).toThrow('Missing parameter "-f <filename>" OR -i <json>')
     })
 
     test('Verify no error message for correct -f parameter is passed in', () => {
-      origProcessArgs.push('-f', 'sample/sample-result.json')
+      // reset process.argv to known state
+      process.argv = []
+      process.argv.push('', '-f', 'sample/sample-result.json')
       expect(getArgs).not.toThrow()
     })
 
     test('Verify no error message for correct -i parameter is passed in', () => {
-      origProcessArgs.push('-i', 'sample/sample-report-format.json')
+      // reset process.argv to known state
+      process.argv = []
+      process.argv.push('', '-i', 'sample/sample-report-format.json')
       expect(getArgs).not.toThrow()
     })
 
     test('Verify no error message for correct -t parameter is passed in', () => {
-      origProcessArgs.push('-t')
+      // reset process.argv to known state
+      // process.agrv = origProcessArgs
+      process.argv = []
+      process.argv.push('', '-t')
       expect(getArgs).not.toThrow()
     })
 
     test('Verify no error message for multiple correct -f, -o parameter', () => {
-      origProcessArgs.push('-f', 'sample/sample-result.json', '-o', 'output/report.html')
+      // reset process.argv to known state
+      process.argv = []
+      process.argv.push('', '-f', 'sample/sample-result.json', '-o', 'output/report.html')
       expect(getArgs).not.toThrow()
     })
 
