d43b00ae8976e0e6f4ffc04c935772fffef370d4
Priyanka Jagga <priyanka.jagga@digio.com.au>
2019-04-02 12:13:52 +1100

    Refactor parameters scenarios for multiple and different options

--- unit.test.js
+++ unit.test.js
@@ -1,60 +1,36 @@
 const {getOptions, getArgs} = require('./utils')
 describe('Unit test for custom report generator', () => {
-  describe('Launch application', () => {
+  describe('Validate different parameters', () => {
     const origProcessArgs = process.argv
 
-    test('Check error message returned when incorrect number of parameters passed in', () => {
-      process.argv.shift()
-
-      function testGetArgs() {
-        getArgs();
-      }
-      expect(testGetArgs).toThrow('Incorrect number of parameters called, expect "-f <filename>" OR -i <json>')
+    test('Error message is returned when incorrect number of parameters passed in', () => {
+      origProcessArgs.shift()
+      expect(getArgs).toThrow('Incorrect number of parameters called, expect "-f <filename>" OR -i <json>')
     })
 
-    test('Check error message returned when incorrect parameters are passed in', () => {
-      process.argv.push('')
-
-      function testGetArgs() {
-        getArgs();
-      }
-      expect(testGetArgs).toThrow('Missing parameter "-f <filename>" OR -i <json>')
+    test('Error is returned for missing parameters', () => {
+      origProcessArgs.push('')
+      expect(getArgs).toThrow('Missing parameter "-f <filename>" OR -i <json>')
     })
 
-    test('Check no error message returned when correct parameters (-f) are passed in', () => {
-      process.argv.push('-f', 'sample/sample-result.json')
-      
-      function testGetArgs() {
-        getArgs();
-      }
-      expect(testGetArgs).not.toThrow()
+    test('Verify no error message for correct -f parameter is passed in', () => {
+      origProcessArgs.push('-f', 'sample/sample-result.json')
+      expect(getArgs).not.toThrow()
     })
 
-    test('Check no error message returned when correct parameters (-i) are passed in', () => {
-      process.argv.push('-i', 'sample/sample-report-format.json')
-      
-      function testGetArgs() {
-        getArgs();
-      }
-      expect(testGetArgs).not.toThrow()
+    test('Verify no error message for correct -i parameter is passed in', () => {
+      origProcessArgs.push('-i', 'sample/sample-report-format.json')
+      expect(getArgs).not.toThrow()
     })
 
-    test('Check no error message returned when correct parameters (-t) are passed in', () => {
-      process.argv.push('-t')
-      
-      function testGetArgs() {
-        getArgs();
-      }
-      expect(testGetArgs).not.toThrow()
+    test('Verify no error message for correct -t parameter is passed in', () => {
+      origProcessArgs.push('-t')
+      expect(getArgs).not.toThrow()
     })
 
-    test('Check no error message returned when correct multiple parameters (-f -o) are passed in', () => {
-      process.argv.push('-f', 'sample/sample-result.json', '-o', 'output/report.html')
-      
-      function testGetArgs() {
-        getArgs();
-      }
-      expect(testGetArgs).not.toThrow()
+    test('Verify no error message for multiple correct -f, -o parameter', () => {
+      origProcessArgs.push('-f', 'sample/sample-result.json', '-o', 'output/report.html')
+      expect(getArgs).not.toThrow()
     })
 
   })
